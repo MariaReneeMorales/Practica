a199d6d398899cf11668fa4949850a087a81c4b3
“[MariaReneeMorales]” <[maria.moralesm@uip.edu.pa]>
2022-03-12 20:07:45 -0600

    Valores Numéricos

--- Librerias.cpp
+++ Librerias.cpp
@@ -8,7 +8,14 @@ int main(){
 	//Permite imprimir tildes, ñ o caracteres especiales. 
 	setlocale(LC_ALL, "");
 	
-	cout << " Hola mundo! Esté es mi primer práctica...";
+	//Definición de variables.
+	float lnNumero1, lnNumero2, lnResultado;
+	
+	//Captura y lectura de datos (valores numéricos).
+	cout << "— Ingrese el primer número: ";
+	cin >> lnNumero1;
+	cout << "— Ingrese el segundo número: ";
+	cin >> lnNumero2;
 	
 	return 0;
 }
