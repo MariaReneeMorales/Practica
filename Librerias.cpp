173ec90712fb2820739ad6dae8079276ccae5cb0
“[MariaReneeMorales]” <[maria.moralesm@uip.edu.pa]>
2022-03-17 06:20:32 -0600

    Números Aleatorios

--- Librerias.cpp
+++ Librerias.cpp
@@ -1,26 +1,18 @@
-#include <stdio.h>
-#include <iostream>
-#include <math.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include<iostream>
 using namespace std;
-
-int main(){
-	//Permite imprimir tildes, ñ o caracteres especiales. 
-	setlocale(LC_ALL, "");
-	
-	//Definición de variables.
-	float lnNumero1, lnNumero2, lnResultado;
-	
-	//Captura y lectura de datos (valores numéricos).
-	cout << "— Ingrese el primer número: ";
-	cin >> lnNumero1;
-	cout << "— Ingrese el segundo número: ";
-	cin >> lnNumero2;
-	
-	//Operación.
-	lnResultado = lnNumero1 / lnNumero2;
-	
-	//Impresión de datos.
-	cout << "— El resultado de " << lnNumero1 << " / " << lnNumero2 << " = " << lnResultado;
-	return 0;
+ 
+int main()
+{
+    int lnPass, lnCount;
+    srand(time(NULL));
+    
+    for(lnCount = 1; lnCount <= 6; lnCount++)
+    {
+        lnPass = 1 + rand() % (11 - 1);
+        cout << lnPass << " ";
+    }
+    
+    return 0;
 }
